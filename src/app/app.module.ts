e45377959bc444205d0ec91a628dac7625a5032b
Dario Barreto <Dario_Barreto@epam.com>
2024-10-11 13:37:13 -0500

    First Commit with lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesModule } from "./countries/countries.module";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+],
   providers: [],
   bootstrap: [AppComponent]
 })
